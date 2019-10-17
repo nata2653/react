047b73daa67251683323369674303c7d2c9076ab
Natalie Sirich <nata2653@stud.kea.dk>
2019-10-17 10:45:02 +0200

    Updates

---
+++ precache-manifest.de0723e14de96bc3e67ba1490d04c5a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0aca9da0017fe0a2cda194bfc8a470",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "42381182c5854715d5ce",
+    "url": "/react/static/css/main.8c6048a0.chunk.css"
+  },
+  {
+    "revision": "3727d78324c0610a3236",
+    "url": "/react/static/js/2.b770d511.chunk.js"
+  },
+  {
+    "revision": "42381182c5854715d5ce",
+    "url": "/react/static/js/main.f2ad9374.chunk.js"
+  },
+  {
+    "revision": "d7db76216c9cc6d5602f",
+    "url": "/react/static/js/runtime-main.9f7e072d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react/static/media/logo.25bf045c.svg"
+  }
+]);
